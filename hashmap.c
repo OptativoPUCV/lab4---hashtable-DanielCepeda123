ea5f2e1816ef25f6af7b5d199d5256fd73958937
Replit user <>
2023-07-01 00:29:40 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -46,25 +46,16 @@ void insertMap(HashMap * map, char * key, void * value) {
   map->current = posicionNuevoDato;
   
   
-  while(map->buckets[map->current] != NULL){
-    if(map->buckets[map->current]->key != NULL){
+  while(map->buckets[map->current] != NULL && map->buckets[map->current]->key != NULL){
       map->current++;
+    if (map->current >= map->capacity) {
+        map->current = 0;
     }
-    
-      if (map->current >= map->capacity) {
-          map->current = 0;
-      }
   }
 
   map->buckets[map->current] = parInsertar;
 
   map->size++;
-
-}
-
-void enlarge(HashMap * map) {
-    enlarge_called = 1; //no borrar (testing purposes)
-
 
 }
 
